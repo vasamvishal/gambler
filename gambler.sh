a4a5b7b058d3620d7817973e4021356c0ad4ea73
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-11-13 10:20:08 +0530

    usercase1 for gambler

--- gambler.sh
+++ gambler.sh
@@ -1,1 +1,19 @@
 #Welcome to the system
+read -p "Enter the no of days:" days
+function bet()
+{
+stakes=100
+for (( count=0;count<$days;count++ ))
+do
+while [ $stakes != 0 ]
+do
+	echo "echo"
+	stakes=$(( $stakes-1 ))
+         echo $stakes
+done
+stakes=100
+
+done
+}
+
+bet 
