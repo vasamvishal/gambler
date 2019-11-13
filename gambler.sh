1b4cab485cbb32a0bfec989034b7bb503af5848a
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-11-13 10:35:27 +0530

    usercase 2 added

--- gambler.sh
+++ gambler.sh
@@ -3,13 +3,26 @@ read -p "Enter the no of days:" days
 function bet()
 {
 stakes=100
+win=1
+loss=0
+bet=0;
 for (( count=0;count<$days;count++ ))
 do
 while [ $stakes != 0 ]
 do
 	echo "echo"
-	stakes=$(( $stakes-1 ))
-         echo $stakes
+	bet=$(( $bet+1 ))
+        random=$((RANDOM%2))
+         if [ $random -eq $win ] 
+       	then
+	echo "You Win"
+          stakes=$(( $stakes+1 ))
+		echo $stakes
+	else
+	echo "You loss"
+    	  stakes=$(( $stakes-1 ))
+ 	echo $stakes
+	fi
 done
 stakes=100
 
