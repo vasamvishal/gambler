df025bcc755e4d05a3a7d640c60095c3c0138925
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-11-13 12:38:29 +0530

    Usercase 3 for checking the valus between 50 and 150

--- gambler.sh
+++ gambler.sh
@@ -1,32 +1,45 @@
 #Welcome to the system
 read -p "Enter the no of days:" days
+min=0;
+max=0;
 function bet()
 {
 stakes=100
+min=$(( $stakes/2 ));
+max=$(( $stakes+min ));
 win=1
 loss=0
-bet=0;
+bet1=100
+
 for (( count=0;count<$days;count++ ))
 do
 while [ $stakes != 0 ]
 do
-	echo "echo"
-	bet=$(( $bet+1 ))
+
         random=$((RANDOM%2))
-         if [ $random -eq $win ] 
-       	then
-	echo "You Win"
-          stakes=$(( $stakes+1 ))
-		echo $stakes
-	else
-	echo "You loss"
-    	  stakes=$(( $stakes-1 ))
- 	echo $stakes
+        if [ $random -eq $win ] 
+        then
+               echo "You Win"
+               stakes=$(( $stakes+1 ))
+	if [ $stakes -eq $max ]
+	then 
+		break;
 	fi
-done
+
+        else
+        echo "You loss"
+	stakes=$(( $stakes-1 ))
+
+	if [ $stakes -eq $min ]
+	then
+         	echo "weee"
+		break;
+	fi
+	fi
+	done
+	
 stakes=100
-
-done
+ done
 }
-
 bet 
+
